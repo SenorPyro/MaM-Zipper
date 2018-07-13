acf217a19f97538d35cf58bb3abd1c1dbbc0cc36
SenorPyro <41166922+SenorPyro@users.noreply.github.com>
2018-07-13 13:03:33 +0200

    update version number

--- MaMzip.user.js
+++ MaMzip.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name				MaM Zipper
 // @namespace			https://nowhere.com/
-// @version				1.0.2b
+// @version				1.0.3b
 // @description			Adds a Download as Zip button to search pages
 // @author				pyrokiller
 // @icon				https://i.imgur.com/ivXsCrU.png
